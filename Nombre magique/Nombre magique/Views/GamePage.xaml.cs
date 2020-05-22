d4fd10952dac2b97017f79aca862167e698811d3
franckyp1 <franckyp1+rapports_git@gmail.com>
2020-05-22 22:18:49 +0200

    Version 4

---
+++ Nombre magique/Nombre magique/Views/GamePage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Nombremagique.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class GamePage : ContentPage
+{
+        const int NB_MAGIQUE_MIN = 1;
+        const int NB_MAGIQUE_MAX = 10;
+        int nombreMagique = 0;
+
+        public GamePage()
+    {
+        InitializeComponent();
+            nombreMagique =new Random().Next(NB_MAGIQUE_MIN, NB_MAGIQUE_MAX);
+            minMaxLabel.Text = "entre " + NB_MAGIQUE_MIN + "et " + NB_MAGIQUE_MAX;
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        void ButtonCliked(object sender, System.EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(numberEntry.Text))
+            {
+                DisplayAlert("Attention !", "Vous devez entrer un nombre", "OK");
+                return;
+            }
+
+            int nombreUtilisateur = 0;
+            try
+            {
+                nombreUtilisateur = Int32.Parse(numberEntry.Text);
+            }
+            catch (Exception){
+                DisplayAlert("Attention !", "Vous devez entrer uniqument des chiffres", "OK");
+                return;
+            }
+
+            if ((nombreUtilisateur < NB_MAGIQUE_MIN) || (nombreUtilisateur > NB_MAGIQUE_MAX)) {
+                DisplayAlert("Attention !", "Vous devez entrer un nombre entre " + NB_MAGIQUE_MIN + " et " + NB_MAGIQUE_MAX, "OK");
+                return;
+            }
+
+            if (nombreMagique > nombreUtilisateur)
+                {
+                    DisplayAlert("Oups !", "Le nombre magique est supérieur à " + nombreUtilisateur, "Ok");
+                    return;
+            }
+
+            if (nombreMagique < nombreUtilisateur)
+                {
+                    DisplayAlert("Oups !", "Le nombre magique est inférieur à " + nombreUtilisateur, "OK");
+                    return;
+            }
+
+            if (nombreMagique == nombreUtilisateur)
+            {
+#pragma warning disable CS4014 // Dans la mesure où cet appel n'est pas attendu, l'exécution de la méthode actuelle continue avant la fin de l'appel
+                WinAction();
+#pragma warning restore CS4014 // Dans la mesure où cet appel n'est pas attendu, l'exécution de la méthode actuelle continue avant la fin de l'appel
+                return;
+
+             }
+            
+        }
+        private async Task WinAction()
+        {
+            await DisplayAlert("BRAVO !", "Vous avez trouvé le nombre magique !", "OK");
+            await this.Navigation.PopAsync();
+        }
+}
+}
---
+++ Nombre magique/Nombre magique/Views/WelcomePage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Nombremagique.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class WelcomePage : ContentPage
+{
+    public WelcomePage()
+    {
+        InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            #pragma warning disable CS4014 // Dans la mesure où cet appel n'est pas attendu, l'exécution de la méthode actuelle continue avant la fin de l'appel
+           
+            InfiniteScale(playButton, 1000);
+            #pragma warning restore CS4014 // Dans la mesure où cet appel n'est pas attendu, l'exécution de la méthode actuelle continue avant la fin de l'appel
+
+        }
+       
+        private async Task InfiniteScale (View view, uint length)
+        {
+            bool always = true;
+
+            while (always)
+            {
+                await view.ScaleTo(1, length);
+                await view.ScaleTo(1.1, length);
+            }
+        }
+    void PlayButtonClicked(object sender, System.EventArgs e)
+    {
+            Navigation.PushAsync(new GamePage());
+    }
+}
+}
